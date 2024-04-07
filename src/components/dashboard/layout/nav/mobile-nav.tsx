c12540c7450d1ae35353a490b7cc0a441de78689
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2024-04-07 06:40:02 +0500

    Create mobile-nav.tsx

---
+++ src/components/dashboard/layout/nav/mobile-nav.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import * as React from 'react';
+import RouterLink from 'next/link';
+import { usePathname } from 'next/navigation';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import Stack from '@mui/material/Stack';
+import { CaretUpDown as CaretUpDownIcon } from '@phosphor-icons/react/dist/ssr/CaretUpDown';
+
+import type { NavItemConfig } from '@/types/nav';
+import { paths } from '@/paths';
+import { Logo } from '@/components/core/logo';
+
+import { navItems } from '../config';
+import NavItem from './item-nav';
+
+export interface MobileNavProps {
+  onClose?: () => void;
+  open?: boolean;
+  items?: NavItemConfig[];
+}
+
+export function MobileNav({ open, onClose }: MobileNavProps): React.JSX.Element {
+  const pathname = usePathname();
+
+  return (
+    <Drawer
+      PaperProps={{
+        sx: {
+          '--MobileNav-background': 'var(--mui-palette-neutral-950)',
+          '--MobileNav-color': 'var(--mui-palette-common-white)',
+          '--NavItem-color': 'var(--mui-palette-neutral-300)',
+          '--NavItem-hover-background': 'rgba(255, 255, 255, 0.04)',
+          '--NavItem-active-background': 'var(--mui-palette-primary-main)',
+          '--NavItem-active-color': 'var(--mui-palette-primary-contrastText)',
+          '--NavItem-disabled-color': 'var(--mui-palette-neutral-500)',
+          '--NavItem-icon-color': 'var(--mui-palette-neutral-400)',
+          '--NavItem-icon-active-color': 'var(--mui-palette-primary-contrastText)',
+          '--NavItem-icon-disabled-color': 'var(--mui-palette-neutral-600)',
+          bgcolor: 'var(--MobileNav-background)',
+          color: 'var(--MobileNav-color)',
+          display: 'flex',
+          flexDirection: 'column',
+          maxWidth: '100%',
+          scrollbarWidth: 'none',
+          width: 'var(--MobileNav-width)',
+          zIndex: 'var(--MobileNav-zIndex)',
+          '&::-webkit-scrollbar': { display: 'none' },
+        },
+      }}
+      onClose={onClose}
+      open={open}
+    >
+      <Stack spacing={2} sx={{ p: 3 }}>
+        <Box component={RouterLink} href={paths.home} sx={{ display: 'inline-flex' }}>
+          <Logo color="light" height={32} width={122} />
+        </Box>
+        <Box
+          sx={{
+            alignItems: 'center',
+            backgroundColor: 'var(--mui-palette-neutral-950)',
+            border: '1px solid var(--mui-palette-neutral-700)',
+            borderRadius: '12px',
+            cursor: 'pointer',
+            display: 'flex',
+            p: '4px 12px',
+          }}
+        >
+          {/* <Box sx={{ flex: '1 1 auto' }}>
+            <Typography color="var(--mui-palette-neutral-400)" variant="body2">
+              Workspace
+            </Typography>
+            <Typography color="inherit" variant="subtitle1">
+              CS Insights
+            </Typography>
+          </Box> */}
+          <CaretUpDownIcon />
+        </Box>
+      </Stack>
+      <Divider sx={{ borderColor: 'var(--mui-palette-neutral-700)' }} />
+      <Box component="nav" sx={{ flex: '1 1 auto', p: '12px' }}>
+        {renderNavItems({ pathname, items: navItems })}
+      </Box>
+    </Drawer>
+  );
+}
+
+function renderNavItems({ items = [], pathname }: { items?: NavItemConfig[]; pathname: string }): React.JSX.Element {
+  const children = items.reduce((acc: React.ReactNode[], curr: NavItemConfig): React.ReactNode[] => {
+    const { key, ...item } = curr;
+
+    acc.push(<NavItem key={key} pathname={pathname} {...item} />);
+
+    return acc;
+  }, []);
+
+  return (
+    <Stack component="ul" spacing={1} sx={{ listStyle: 'none', m: 0, p: 0 }}>
+      {children}
+    </Stack>
+  );
+}
