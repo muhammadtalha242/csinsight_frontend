bf11890d0a6d77642d13497a24e4259040ad1255
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2024-04-07 06:39:35 +0500

    Navbar items fixed

---
+++ src/components/dashboard/layout/nav/item-nav.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import * as React from 'react';
+import RouterLink from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+
+import type { NavItemConfig } from '@/types/nav';
+
+import { navIcons } from './nav-icons';
+
+interface NavItemProps extends Omit<NavItemConfig, 'items'> {
+  pathname: string;
+}
+
+export default function NavItem({ disabled, external, href, icon, title }: NavItemProps): React.JSX.Element {
+  const router = usePathname();
+  const isActive = router === href;
+
+  const Icon = icon ? navIcons[icon] : null;
+  return (
+    <Box
+      {...(!disabled && href
+        ? {
+            component: RouterLink,
+            href,
+            target: external ? '_blank' : undefined,
+            rel: external ? 'noreferrer' : undefined,
+          }
+        : { role: 'button' })}
+      sx={{
+        alignItems: 'center',
+        borderRadius: 1,
+        color: 'var(--NavItem-color)',
+        cursor: 'pointer',
+        display: 'flex',
+        flex: '0 0 auto',
+        gap: 1,
+        p: '6px 16px',
+        position: 'relative',
+        textDecoration: 'none',
+        whiteSpace: 'nowrap',
+        ...(disabled && {
+          bgcolor: 'var(--NavItem-disabled-background)',
+          color: 'var(--NavItem-disabled-color)',
+          cursor: 'not-allowed',
+        }),
+        ...(isActive && {
+          bgcolor: 'var(--NavItem-active-background)',
+          color: 'var(--NavItem-active-color)',
+          border: '2px solid',
+        }),
+      }}
+    >
+      <Box sx={{ alignItems: 'center', display: 'flex', justifyContent: 'center', flex: '0 0 auto' }}>
+        {Icon ? (
+          <Icon
+            fill={isActive ? 'var(--NavItem-icon-active-color)' : 'var(--NavItem-icon-color)'}
+            fontSize="var(--icon-fontSize-md)"
+            weight={isActive ? 'fill' : undefined}
+          />
+        ) : null}
+      </Box>
+      <Box sx={{ flex: '1 1 auto' }}>
+        <Typography
+          component="span"
+          sx={{ color: 'inherit', fontSize: '0.875rem', fontWeight: 500, lineHeight: '28px' }}
+        >
+          {title}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
--- src/components/dashboard/layout/nav/nav-icons.tsx
+++ src/components/dashboard/layout/nav/nav-icons.tsx
@@ -1,15 +1,17 @@
 import type { Icon } from '@phosphor-icons/react/dist/lib/types';
 import { ChartPie as ChartPieIcon } from '@phosphor-icons/react/dist/ssr/ChartPie';
 import { GearSix as GearSixIcon } from '@phosphor-icons/react/dist/ssr/GearSix';
-import { PlugsConnected as PlugsConnectedIcon } from '@phosphor-icons/react/dist/ssr/PlugsConnected';
+import { MapPin } from '@phosphor-icons/react/dist/ssr/MapPin';
+import { Scroll } from '@phosphor-icons/react/dist/ssr/Scroll';
 import { User as UserIcon } from '@phosphor-icons/react/dist/ssr/User';
 import { Users as UsersIcon } from '@phosphor-icons/react/dist/ssr/Users';
 import { XSquare } from '@phosphor-icons/react/dist/ssr/XSquare';
 
 export const navIcons = {
   'chart-pie': ChartPieIcon,
+  description: Scroll,
   'gear-six': GearSixIcon,
-  'plugs-connected': PlugsConnectedIcon,
+  MapPin,
   'x-square': XSquare,
   user: UserIcon,
   users: UsersIcon,
