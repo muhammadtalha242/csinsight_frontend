07d743c096cf8ba80bae29294f78393654e70cf7
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2024-04-10 23:44:08 +0500

    Update distributionsOverTime.tsx

--- src/components/dashboard/overview/distributionsOverTime.tsx
+++ src/components/dashboard/overview/distributionsOverTime.tsx
@@ -1,22 +1,34 @@
 'use client';
 
-import React from 'react';
+import React, { useContext } from 'react';
+import visualizationsService, { type getPapersCountsPostResponse } from '@/services/visualizations';
+import { useQuery } from 'react-query';
 
+import { FilterContext } from '@/contexts/filters.context';
 import { LineChart } from '@/components/visualization/lineChart';
 
-import sampleData from './sample.data.json';
-
 const DistributionsOverTime = () => {
+  const { state: filterState } = useContext(FilterContext);
+
   const graphData: number[] = [];
   const xAxisCategories: string[] = [];
 
-  sampleData.forEach((v: { count: number; totalCitations: number; year: string }) => {
-    graphData.push(v.totalCitations);
-    xAxisCategories.push(v.year);
-  });
+  const { data, isLoading, error } = useQuery<getPapersCountsPostResponse[], Error>(
+    ['myData2', filterState.filters],
+    () => visualizationsService.getPapersCountPost(filterState.filters)
+  );
+
+  if (!isLoading && data) {
+    data.forEach((v: getPapersCountsPostResponse) => {
+      graphData.push(v.totalCitations);
+      xAxisCategories.push(v.year);
+    });
+  }
+  if (error) return <>MISSING DATA</>;
 
   return (
     <LineChart
+      isLoading={isLoading}
       chartSeries={[{ name: 'This year', data: graphData }]}
       sx={{ height: '100%' }}
       title="Bar Chart"
