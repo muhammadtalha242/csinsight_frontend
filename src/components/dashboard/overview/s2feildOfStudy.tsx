877cc0a46bf18531cb1ebb0c680349aa0722f988
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2024-04-12 10:00:12 +0500

    Update s2feildOfStudy.tsx

--- src/components/dashboard/overview/s2feildOfStudy.tsx
+++ src/components/dashboard/overview/s2feildOfStudy.tsx
@@ -1,20 +1,38 @@
 'use client';
 
-import React from 'react';
+import React, { useContext } from 'react';
+import visualizationsService, { type getS2FeildResponse } from '@/services/visualizations';
+import { useQuery } from 'react-query';
 
+import { FilterContext } from '@/contexts/filters.context';
 import { DonutChart } from '@/components/visualization/donutChart';
 
-import sampleData from './s2f.data.json';
-
 const S2FeidlOfStudy = () => {
+  const { state: filterState } = useContext(FilterContext);
   const graphData: number[] = [];
   const xAxisCategories: string[] = [];
-  sampleData.forEach((v: { category: string; category_count: number }) => {
-    graphData.push(v.category_count);
-    xAxisCategories.push(v.category);
-  });
 
-  return <DonutChart chartSeries={graphData} sx={{ height: '100%' }} title="S2Field Chart" labels={xAxisCategories} />;
+  const { data, isLoading } = useQuery<getS2FeildResponse[], Error>(['s2F', filterState.filters], () =>
+    visualizationsService.getS2Feilds({
+      ...filterState.filters,
+    })
+  );
+
+  if (!isLoading && data) {
+    data.forEach((v: getS2FeildResponse) => {
+      graphData.push(Number(v.category_count));
+      xAxisCategories.push(v.category);
+    });
+  }
+  return (
+    <DonutChart
+      chartSeries={graphData}
+      sx={{ height: '100%' }}
+      title="S2Field Chart"
+      labels={xAxisCategories}
+      isLoading={isLoading}
+    />
+  );
 };
 
 export default S2FeidlOfStudy;
