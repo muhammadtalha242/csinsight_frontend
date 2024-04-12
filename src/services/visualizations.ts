245e3101275f657ebac403dc2b8675a7cf3c6af7
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2024-04-12 10:00:21 +0500

    Update visualizations.ts

--- src/services/visualizations.ts
+++ src/services/visualizations.ts
@@ -1,8 +1,8 @@
 import { API_BASE_URL } from '@/constants/const';
-import { getUrlSearchParams } from '@/util/getSearchParams';
 import axios from 'axios';
 
-import { type Filter } from '@/types/filters';
+import { type Filter, type PagedParameters } from '@/types/filters';
+import type { Data } from '@/components/dashboard/paper/TopPapers';
 
 export interface getPapersCountsPostResponse {
   count: number;
@@ -10,25 +10,33 @@ export interface getPapersCountsPostResponse {
   year: string;
 }
 
-const getPapersCount = async ({ queryKey }: any) => {
-  const [, filters] = queryKey;
+export interface getTopPapersResponse {
+  rowCount: number;
+  rows: Data[];
+}
 
-  console.log('queryKey', queryKey);
-  const queryParams = getUrlSearchParams(filters);
-
-  const res = await axios.get(`${API_BASE_URL}/papers/years?${queryParams}`);
-
-  return res.data;
-};
+export interface getS2FeildResponse { category: string; category_count: number }
 
 const getPapersCountPost = async (queryParams: Filter): Promise<getPapersCountsPostResponse[]> => {
-  console.log('queryParams', queryParams);
   const res = await axios.post<getPapersCountsPostResponse[]>(`${API_BASE_URL}/papers/years`, queryParams);
 
   return res.data;
 };
 
+const getTopPapers = async (queryParams: Filter & PagedParameters): Promise<getTopPapersResponse> => {
+  const res = await axios.post<getTopPapersResponse>(`${API_BASE_URL}/papers/info`, queryParams);
+
+  return res.data;
+};
+
+const getS2Feilds = async (queryParams: Filter ): Promise<getS2FeildResponse[]> => {
+  const res = await axios.post<getS2FeildResponse[]>(`${API_BASE_URL}/papers/s2f`, queryParams);
+
+  return res.data;
+};
+
 export default {
-  getPapersCount,
+  getTopPapers,
   getPapersCountPost,
+  getS2Feilds,
 };
