83150252947943686982cd77065a6a1ae7922de5
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2024-04-12 10:00:15 +0500

    Update autocomplete.ts

--- src/services/autocomplete.ts
+++ src/services/autocomplete.ts
@@ -1,5 +1,5 @@
 import { API_BASE_URL, type AUTOCOMPLETE_ROUTES } from '@/constants/const';
-import axios, { type CancelTokenSource } from 'axios';
+import axios from 'axios';
 import { useQuery } from 'react-query';
 
 interface AutocompleteProps {
@@ -8,29 +8,15 @@ interface AutocompleteProps {
 }
 
 export const useAutocomplete = ({ route, inputValue }: AutocompleteProps) => {
-  console.log('inputValue', inputValue);
-
-  let cancelTokenSource: CancelTokenSource | null = null;
 
   const autocomplete = async () => {
     if (!inputValue) return [];
 
-    // Cancel previous request if it exists
-
-    if (cancelTokenSource) {
-      cancelTokenSource.cancel('Request canceled');
-    }
-
-    // Create a new cancel token source
-    cancelTokenSource = axios.CancelToken.source();
-
     try {
-      const response = await axios.get(`${API_BASE_URL}/autocomplete/${route}?q=${inputValue}`, {
-        cancelToken: cancelTokenSource.token,
-      });
+      const response = await axios.get<{id: string, value: 'string'}[]>(`${API_BASE_URL}/autocomplete/${route}?q=${inputValue}`);
 
       return response.data.map((item: { id: string; value: string }) => ({
-        key: item.id,
+        label: item.id,
         value: item.value,
       }));
     } catch (error) {
