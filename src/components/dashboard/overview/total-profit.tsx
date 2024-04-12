25abb626cafec509b248f7c3ee359bc46bdc4331
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2024-04-12 10:00:13 +0500

    Update total-profit.tsx

--- src/components/dashboard/overview/total-profit.tsx
+++ src/components/dashboard/overview/total-profit.tsx
@@ -5,7 +5,7 @@ import CardContent from '@mui/material/CardContent';
 import Stack from '@mui/material/Stack';
 import type { SxProps } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import { Receipt as ReceiptIcon } from '@phosphor-icons/react/dist/ssr/Receipt';
+import { MapPin } from '@phosphor-icons/react/dist/ssr/MapPin';
 
 export interface TotalProfitProps {
   sx?: SxProps;
@@ -19,12 +19,12 @@ export function TotalProfit({ value, sx }: TotalProfitProps): React.JSX.Element 
         <Stack direction="row" sx={{ alignItems: 'flex-start', justifyContent: 'space-between' }} spacing={3}>
           <Stack spacing={1}>
             <Typography color="text.secondary" variant="overline">
-              Total Profit
+              Total Venues
             </Typography>
             <Typography variant="h4">{value}</Typography>
           </Stack>
           <Avatar sx={{ backgroundColor: 'var(--mui-palette-primary-main)', height: '56px', width: '56px' }}>
-            <ReceiptIcon fontSize="var(--icon-fontSize-lg)" />
+            <MapPin fontSize="var(--icon-fontSize-lg)" />
           </Avatar>
         </Stack>
       </CardContent>
