b6fdea5aeb446759c3ea6948287a331663b3401b
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2024-04-14 01:55:43 +0500

    Update config.ts

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -5,7 +5,7 @@ export const navItems = [
   // { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie', disabled: true },
   { key: 'papers', title: 'Papers', href: paths.dashboard.papers, icon: 'description' },
   { key: 'authors', title: 'Authors', href: paths.dashboard.authors, icon: 'users' },
-  { key: 'venues', title: 'Venues', href: paths.dashboard.integrations, icon: 'MapPin' },
+  { key: 'venues', title: 'Venues', href: paths.dashboard.venues, icon: 'MapPin' },
   // { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
   // { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
   // { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
