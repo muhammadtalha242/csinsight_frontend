b1303b1c9842611270dab725e9022d07f82a9a0e
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2024-04-14 01:56:04 +0500

    Update visualizations.ts

--- src/services/visualizations.ts
+++ src/services/visualizations.ts
@@ -2,7 +2,9 @@ import { API_BASE_URL } from '@/constants/const';
 import axios from 'axios';
 
 import { type Filter, type PagedParameters } from '@/types/filters';
+import type { AuthorData } from '@/components/dashboard/author/AuthorsTable';
 import type { Data } from '@/components/dashboard/paper/TopPapers';
+import type { VenueData } from '@/components/dashboard/venue/VenuesTable';
 
 export interface getPapersCountsPostResponse {
   count: number;
@@ -15,7 +17,20 @@ export interface getTopPapersResponse {
   rows: Data[];
 }
 
-export interface getS2FeildResponse { category: string; category_count: number }
+export interface getAuthorsResponse {
+  rowCount: number;
+  rows: AuthorData[];
+}
+
+export interface getVenuesResponse {
+  rowCount: number;
+  rows: VenueData[];
+}
+
+export interface getS2FeildResponse {
+  category: string;
+  category_count: number;
+}
 
 const getPapersCountPost = async (queryParams: Filter): Promise<getPapersCountsPostResponse[]> => {
   const res = await axios.post<getPapersCountsPostResponse[]>(`${API_BASE_URL}/papers/years`, queryParams);
@@ -29,8 +44,20 @@ const getTopPapers = async (queryParams: Filter & PagedParameters): Promise<getT
   return res.data;
 };
 
-const getS2Feilds = async (queryParams: Filter ): Promise<getS2FeildResponse[]> => {
+const getS2Feilds = async (queryParams: Filter): Promise<getS2FeildResponse[]> => {
   const res = await axios.post<getS2FeildResponse[]>(`${API_BASE_URL}/papers/s2f`, queryParams);
+
+  return res.data;
+};
+
+const getAuthors = async (queryParams: Filter & PagedParameters): Promise<getAuthorsResponse> => {
+  const res = await axios.post<getAuthorsResponse>(`${API_BASE_URL}/authors/info`, queryParams);
+
+  return res.data;
+};
+
+const getVenues = async (queryParams: Filter & PagedParameters): Promise<getVenuesResponse> => {
+  const res = await axios.post<getVenuesResponse>(`${API_BASE_URL}/venues/info`, queryParams);
 
   return res.data;
 };
@@ -39,4 +66,6 @@ export default {
   getTopPapers,
   getPapersCountPost,
   getS2Feilds,
+  getAuthors,
+  getVenues,
 };
